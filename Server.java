b5a0561cc6102807df0e4020cc59bc1bf94d8632
Infectedwafle <andersk82@yahoo.com>
2015-11-30 22:32:34 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -11,64 +11,81 @@ import java.net.*;
 import java.nio.file.Files;
 //Server class for the client to connect to.
 public class Server {
-	private String method = null;
-	private String path = null;
-	private String protocol = null;	
-	private int clientNum;
-	@SuppressWarnings("resource")
+	static int received = 0;
+	static int sent = 0;
+	DatagramSocket serverSocket = new DatagramSocket(9875);
+	static String fileName;
 	public Server() throws IOException{
-		DatagramSocket serverSocket = new DatagramSocket(9876);
+		
 		while(true) {
-			byte[] recieveData = new byte[512];
+			byte[] recieveData = new byte[1024];
 			DatagramPacket recievePacket = new DatagramPacket(recieveData, recieveData.length);
-			serverSocket.recieve(recievePacket);
+			serverSocket.receive(recievePacket);
 
 			// the message recieved from the client
-			String message = new String(recieveData);
-
+			fileName = new String(recievePacket.getData()).trim();
 			InetAddress ipAddress = recievePacket.getAddress();
 			int port = recievePacket.getPort();
-
-			System.out.println(message);
+			
+			serverSocket.connect(ipAddress, port);
+//			byte[] sendData = message.getBytes();
+//			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
+//			serverSocket.send(sendPacket);
+//			This method takes the request from the client to find and send the file.
+			
+			
+			
+			
+				
+			Thread sendThread = new Thread(){
+				public void run(){
+					System.out.println("Thread Running");
+					InputStream fileIn = null;
+					try{
+				     	File file = new File ("Data/" + fileName);
+						fileIn = new FileInputStream(file);	
+						byte[] sendData = new byte[1024];
+						int amountRead = fileIn.read(sendData);
+						
+						while(amountRead > 0){
+							if((sent - received) < 5) {
+								DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
+								serverSocket.send(sendPacket);
+								amountRead = fileIn.read(sendData);
+							}
+						}
+						fileIn.close();
+						sendData = new byte[1024];
+						sendData = new String("end-of-file").getBytes();
+						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
+						serverSocket.send(sendPacket);
+					} catch (IOException e){
+						//output.println("File does not exist");
+						System.out.println("File not found...");	    
+					}
+				}
+			};
+			
+			Thread receiveThread = new Thread(){
+				public void run(){
+					System.out.println("Thread Running");
+					try{
+						while(serverSocket.isConnected()){
+							byte[] receiveData = new byte[1024];
+							DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+							serverSocket.receive(receivePacket);
+							received++;
+							System.out.println("Acknowledgement Received: "  + received);
+						}
+					} catch (IOException e){
+						//output.println("File does not exist");
+						System.out.println("File not found...");	    
+					}
+				}
+			};
+			  
+			sendThread.start();
+			receiveThread.start();					
 		}	    
-	}
-	
-	// This method handles the socket connection from the clients
-	public void handleConnection(Socket socket) throws IOException{
-		BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));    
-		String line;    
-		line = input.readLine();
-		System.out.println(line);
-		if(line != null){
-			  processRequest(line, socket);
-		}    
-
-		input.close();
-		socket.close();
-		clientNum--;
-	}
-	
-	//This method takes the request from the client to find and send the file. 
-	public void processRequest(String fileName, Socket socket) throws IOException{
-		PrintStream output = new PrintStream(socket.getOutputStream(), true);
-		InputStream fileIn = null;
-		try{
-			File file = new File ("../Data/" + fileName);
-			fileIn = new FileInputStream(file);	
-			byte[] buffer = new byte[1024];
-			int amountRead = fileIn.read(buffer);
-			while(amountRead > 0){
-				output.write(buffer, 0, amountRead); // write data back out to an OutputStream
-				amountRead = fileIn.read(buffer);
-			}
-			output.println("");
-			fileIn.close();
-			output.println("");
-			System.out.println("File transfered.");
-		} 
-		catch (FileNotFoundException e){
-			output.println("File does not exist");
-			System.out.println("File not found...");	    
-		}
 	}
 }
