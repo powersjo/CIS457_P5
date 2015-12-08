2339030eae76fdb9a9054b7d86f191637fa6b05d
Infectedwafle <andersk82@yahoo.com>
2015-12-08 01:26:17 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,251 +6,346 @@ Jonathan Powers, Kevin Anderson, Brett Greenman
  */
 
 import java.io.*;
+import java.util.*;
 import java.math.BigInteger;
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
+
 //Server class for the client to connect to.
 public class Server {
 	static boolean thread1, thread2, thread3, thread4, thread5;
 	static String fileName;
-        static InputStream fileIn;
+	static InputStream fileIn;
 	static InetAddress ipAddress;
 	static int port;
 	static File file;
+	static int packetNumber;
+	static DatagramSocket serverSocket;
+
 	public Server() throws IOException{
 		thread1 = thread2 = thread3 = thread4 = thread5 = false;
-		while(true) {
-			DatagramSocket serverSocket = new DatagramSocket(9876);
+		serverSocket = new DatagramSocket(9999);
+		//serverSocket.setSoTimeout(30000);
+		//while(true) {
 			byte[] recieveData = new byte[1024];
 			DatagramPacket recievePacket = new DatagramPacket(recieveData, recieveData.length);
 			serverSocket.receive(recievePacket);
-
-			serverSocket.setSoTimeout(1000);
 			// the message recieved from the client
 			fileName = new String(recievePacket.getData()).trim();
 			ipAddress = recievePacket.getAddress();
 			port = recievePacket.getPort();
-			
-			serverSocket.close();
-//			byte[] sendData = message.getBytes();
-//			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-//			serverSocket.send(sendPacket);
-//			This method takes the request from the client to find and send the file.
-			
-			
 			file = new File ("Data/" + fileName);
 			fileIn = new FileInputStream(file);
+			packetNumber = 0;
+			
+			//serverSocket.disconnect();
 
 			Thread sendThread1 = new Thread(){
 				public void run(){
 					System.out.println("Thread 1 Running");
-					//DatagramSocket threadSocket1 = new DatagramSocket();
-					//threadSocket1.connect(ipAddress, port);
-					//InputStream fileIn = null;
 					try{
-						DatagramSocket threadSocket1 = new DatagramSocket();
-						threadSocket1.connect(ipAddress, port);
-						byte[] read1b = new byte[8]; //to add to packet to indicate thread num.
-				     	//File file = new File ("Data/" + fileName);
-						//fileIn = new FileInputStream(file);	
-						byte[] sendData = new byte[1024];
-						int amountRead = fileIn.read(sendData);
+						//serverSocket = new DatagramSocket();
+						//serverSocket.connect(ipAddress, port);
+						serverSocket.setSoTimeout(3000);
+						byte[] read1b; //to add to packet to indicate packet sequence number.
+						byte[] send1b = new byte[1020];
+						int amountRead = fileIn.read(send1b);
+						boolean send = true;
 						
 						while(amountRead > 0){
-							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-							threadSocket1.send(sendPacket);
-							amountRead = fileIn.read(sendData);
-							//while(){
-							  byte[] receiveData = new byte[1024];
-							  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-							  try{
-							    threadSocket1.receive(receivePacket);
-							  } catch (SocketTimeoutException e){
-							    threadSocket1.send(sendPacket);
-							    continue;
-							  }
-							  System.out.println("Acknowledgement Received");
-							//}
+							packetNumber++;
+							if(amountRead != send1b.length) {
+								send1b = Arrays.copyOf(send1b, amountRead);
+							}
+							read1b = intToByteArray(packetNumber);
+							byte[] sendData = concatByteArray(read1b, send1b);
+							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+							
+							System.out.println("Sent Packet: " + amountRead + "|" + send1b.length);
+							serverSocket.send(sendPacket);
+
+							send = false;
+							while(!send){
+								  byte[] receiveData = new byte[64];
+								  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+								  try{
+									  //System.out.println("Waiting for Packet");
+									  serverSocket.receive(receivePacket);
+									  if(new String(receivePacket.getData()).length() > 0) {
+										  //System.out.println("Acknowledgement Received");
+										  amountRead = fileIn.read(send1b);
+										  send = true;
+									  }
+								  } catch (SocketTimeoutException e){
+									  	System.out.println("Resending Packet");
+									  	serverSocket.send(sendPacket);
+								    	send = false;
+								    	continue;
+								  }								  
+							}
 						}
-						//fileIn.close();
-						sendData = new byte[1024];
-						sendData = new String("end-of-file").getBytes();
-						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-						threadSocket1.send(sendPacket);
+						thread1 = true;
 					} catch (SocketTimeoutException e){
 						System.out.println(e.getStackTrace());		    
-					} catch (IOException e){}
+					} catch (IOException e){
+						System.out.println(e.getStackTrace());
+					}
 				}
 			};
 	
 			Thread sendThread2 = new Thread(){
 				public void run(){
 					System.out.println("Thread 2 Running");
-					//DatagramSocket threadSocket2 = new DatagramSocket();
-					//threadSocket2.connect(ipAddress, port);
-					//InputStream fileIn = null;
 					try{
-						DatagramSocket threadSocket2 = new DatagramSocket();
-						threadSocket2.connect(ipAddress, port);
-						byte[] read2b = new byte[8];
-				     	//File file = new File ("Data/" + fileName);
-						//fileIn = new FileInputStream(file);	
-						byte[] sendData = new byte[1024];
-						int amountRead = fileIn.read(sendData);
+						//serverSocket = new DatagramSocket();
+						//serverSocket.connect(ipAddress, port);
+						serverSocket.setSoTimeout(3000);
+						byte[] read2b; //to add to packet to indicate packet sequence number.
+						byte[] send2b = new byte[1020];
+						int amountRead = fileIn.read(send2b);
+						boolean send = true;
 						
 						while(amountRead > 0){
-							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-							threadSocket2.send(sendPacket);
-							amountRead = fileIn.read(sendData);
-  
-                                                        byte[] receiveData = new byte[1024];
-							DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-							threadSocket2.receive(receivePacket);
-							System.out.println("Acknowledgement Received");
+							packetNumber++;
+							if(amountRead != send2b.length) {
+								send2b = Arrays.copyOf(send2b, amountRead);
+							}
+							read2b = intToByteArray(packetNumber);
+							byte[] sendData = concatByteArray(read2b, send2b);
+							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+							
+							System.out.println("Sent Packet: " + amountRead + "|" + send2b.length);
+							serverSocket.send(sendPacket);
+							send = false;
+							while(!send){
+								  byte[] receiveData = new byte[64];
+								  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+								  try{
+									  //System.out.println("Waiting for Packet");
+									  serverSocket.receive(receivePacket);
+									  if(new String(receivePacket.getData()).length() > 0) {
+										  //System.out.println("Acknowledgement Received");
+										  amountRead = fileIn.read(send2b);
+										  send = true;
+									  }
+								  } catch (SocketTimeoutException e){
+									  	System.out.println("Resending Packet");
+									  	serverSocket.send(sendPacket);
+								    	send = false;
+								    	continue;
+								  }								  
+							}
 						}
-						//fileIn.close();
-						sendData = new byte[1024];
-						sendData = new String("end-of-file").getBytes();
-						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-						threadSocket2.send(sendPacket);
+						thread2 = true;
 					} catch (SocketTimeoutException e){
 						System.out.println(e.getStackTrace());		    
-					} catch (IOException e){}
+					} catch (IOException e){
+						System.out.println(e.getStackTrace());
+					}
 				}
 			};
 
 			Thread sendThread3 = new Thread(){
 				public void run(){
-					System.out.println("Thread 3 Running");
-					//DatagramSocket threadSocket3 = new DatagramSocket();
-					//threadSocket3.connect(ipAddress, port);
-					//InputStream fileIn = null;
+					System.out.println("Thread 2 Running");
 					try{
-						DatagramSocket threadSocket3 = new DatagramSocket();
-						threadSocket3.connect(ipAddress, port);
-						byte[] read3b = new byte[8];
-						//File file = new File ("Data/" + fileName);
-						//fileIn = new FileInputStream(file);	
-						byte[] sendData = new byte[1024];
-						int amountRead = fileIn.read(sendData);
+						//serverSocket = new DatagramSocket();
+						//serverSocket.connect(ipAddress, port);
+						serverSocket.setSoTimeout(3000);
+						byte[] read3b; //to add to packet to indicate packet sequence number.
+						byte[] send3b = new byte[1020];
+						int amountRead = fileIn.read(send3b);
+						boolean send = true;
 						
 						while(amountRead > 0){
-							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-							threadSocket3.send(sendPacket);
-							amountRead = fileIn.read(sendData);
-  
-                                                        byte[] receiveData = new byte[1024];
-							DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-							threadSocket3.receive(receivePacket);
-							System.out.println("Acknowledgement Received");
+							packetNumber++;
+							if(amountRead != send3b.length) {
+								send3b = Arrays.copyOf(send3b, amountRead);
+							}
+							read3b = intToByteArray(packetNumber);
+							byte[] sendData = concatByteArray(read3b, send3b);
+							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+							
+							System.out.println("Sent Packet: " + amountRead + "|" + send3b.length);
+							serverSocket.send(sendPacket);
+							send = false;
+							while(!send){
+								  byte[] receiveData = new byte[64];
+								  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+								  try{
+									  //System.out.println("Waiting for Packet");
+									  serverSocket.receive(receivePacket);
+									  if(new String(receivePacket.getData()).length() > 0) {
+										  //System.out.println("Acknowledgement Received");
+										  amountRead = fileIn.read(send3b);
+										  send = true;
+									  }
+								  } catch (SocketTimeoutException e){
+									  	System.out.println("Resending Packet");
+									  	serverSocket.send(sendPacket);
+								    	send = false;
+								    	continue;
+								  }								  
+							}
 						}
-						//fileIn.close();
-						sendData = new byte[1024];
-						sendData = new String("end-of-file").getBytes();
-						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-						threadSocket3.send(sendPacket);
+						thread3 = true;
 					} catch (SocketTimeoutException e){
 						System.out.println(e.getStackTrace());		    
-					} catch (IOException e){}
+					} catch (IOException e){
+						System.out.println(e.getStackTrace());
+					}
 				}
 			};
 
 			Thread sendThread4 = new Thread(){
 				public void run(){
-					System.out.println("Thread 4 Running");
-					//DatagramSocket threadSocket4 = new DatagramSocket();
-					//threadSocket4.connect(ipAddress, port);
-					//InputStream fileIn = null;
+					System.out.println("Thread 2 Running");
 					try{
-						DatagramSocket threadSocket4 = new DatagramSocket();
-						threadSocket4.connect(ipAddress, port);
-						byte[] read4b = new byte[8];
-				     	//File file = new File ("Data/" + fileName);
-						//fileIn = new FileInputStream(file);	
-						byte[] sendData = new byte[1024];
-						int amountRead = fileIn.read(sendData);
+						//serverSocket = new DatagramSocket();
+						//serverSocket.connect(ipAddress, port);
+						serverSocket.setSoTimeout(3000);
+						byte[] read4b; //to add to packet to indicate packet sequence number.
+						byte[] send4b = new byte[1020];
+						int amountRead = fileIn.read(send4b);
+						boolean send = true;
 						
 						while(amountRead > 0){
-							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-							threadSocket4.send(sendPacket);
-							amountRead = fileIn.read(sendData);
-  
-                                                        byte[] receiveData = new byte[1024];
-							DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-							threadSocket4.receive(receivePacket);
-							System.out.println("Acknowledgement Received");
+							packetNumber++;
+							if(amountRead != send4b.length) {
+								send4b = Arrays.copyOf(send4b, amountRead);
+							}
+							read4b = intToByteArray(packetNumber);
+							byte[] sendData = concatByteArray(read4b, send4b);
+							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+							
+							System.out.println("Sent Packet: " + amountRead + "|" + send4b.length);
+							serverSocket.send(sendPacket);
+							send = false;
+							while(!send){
+								  byte[] receiveData = new byte[64];
+								  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+								  try{
+									  //System.out.println("Waiting for Packet");
+									  serverSocket.receive(receivePacket);
+									  if(new String(receivePacket.getData()).length() > 0) {
+										  //System.out.println("Acknowledgement Received");
+										  amountRead = fileIn.read(send4b);
+										  send = true;
+									  }
+								  } catch (SocketTimeoutException e){
+									  	System.out.println("Resending Packet");
+									  	serverSocket.send(sendPacket);
+								    	send = false;
+								    	continue;
+								  }								  
+							}
 						}
-						//fileIn.close();
-						sendData = new byte[1024];
-						sendData = new String("end-of-file").getBytes();
-						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-						threadSocket4.send(sendPacket);
+						thread4 = true;
 					} catch (SocketTimeoutException e){
-						System.out.println(e.getStackTrace());	    
-					} catch (IOException e){}
+						System.out.println(e.getStackTrace());		    
+					} catch (IOException e){
+						System.out.println(e.getStackTrace());
+					}
 				}
 			};
 
 			Thread sendThread5 = new Thread(){
 				public void run(){
-					System.out.println("Thread 5 Running");
-					//DatagramSocket threadSocket5 = new DatagramSocket();
-					//threadSocket5.connect(ipAddress, port);
-					//InputStream fileIn = null;
+					System.out.println("Thread 2 Running");
 					try{
-						DatagramSocket threadSocket5 = new DatagramSocket();
-						threadSocket5.connect(ipAddress, port);
-						byte[] read5b = new byte[8];
-				     	//File file = new File ("Data/" + fileName);
-						//fileIn = new FileInputStream(file);	
-						byte[] sendData = new byte[1024];
-						int amountRead = fileIn.read(sendData);
+						//serverSocket = new DatagramSocket();
+						//serverSocket.connect(ipAddress, port);
+						serverSocket.setSoTimeout(3000);
+						byte[] read5b; //to add to packet to indicate packet sequence number.
+						byte[] send5b = new byte[1020];
+						int amountRead = fileIn.read(send5b);
+						boolean send = true;
 						
 						while(amountRead > 0){
-							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-							threadSocket5.send(sendPacket);
-							amountRead = fileIn.read(sendData);
-  
-                                                        byte[] receiveData = new byte[1024];
-							DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-							threadSocket5.receive(receivePacket);
-							System.out.println("Acknowledgement Received");
+							packetNumber++;
+							if(amountRead != send5b.length) {
+								send5b = Arrays.copyOf(send5b, amountRead);
+							}
+							read5b = intToByteArray(packetNumber);
+							byte[] sendData = concatByteArray(read5b, send5b);
+							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+							
+							System.out.println("Sent Packet: " + amountRead + "|" + send5b.length);
+							serverSocket.send(sendPacket);
+							send = false;
+							while(!send){
+								  byte[] receiveData = new byte[64];
+								  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+								  try{
+									  //System.out.println("Waiting for Packet");
+									  serverSocket.receive(receivePacket);
+									  if(new String(receivePacket.getData()).length() > 0) {
+										  //System.out.println("Acknowledgement Received");
+										  amountRead = fileIn.read(send5b);
+										  send = true;
+									  }
+								  } catch (SocketTimeoutException e){
+									  	System.out.println("Resending Packet");
+									  	serverSocket.send(sendPacket);
+								    	send = false;
+								    	continue;
+								  }								  
+							}
 						}
-						//fileIn.close();
-						sendData = new byte[1024];
-						sendData = new String("end-of-file").getBytes();
-						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
-						threadSocket5.send(sendPacket);
+
+						thread5 = true;
 					} catch (SocketTimeoutException e){
-						System.out.println(e.getStackTrace());	    
-					} catch (IOException e){}
-				}
-			};
-			
-			/*Thread receiveThread = new Thread(){
-				public void run(){
-					System.out.println("Thread Running");
-					try{
-						while(serverSocket.isConnected()){
-							byte[] receiveData = new byte[1024];
-							DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-							serverSocket.receive(receivePacket);
-							received++;
-							System.out.println("Acknowledgement Received: "  + received);
-						}
+						System.out.println(e.getStackTrace());		    
 					} catch (IOException e){
-						//output.println("File does not exist");
-						System.out.println("File not found...");	    
+						System.out.println(e.getStackTrace());
 					}
 				}
-			};*/
+			};
 			  
 			sendThread1.start();
 			sendThread2.start();
 			sendThread3.start();
 			sendThread4.start();
 			sendThread5.start();
-			//receiveThread.start();					
-		}	    
+
+			while(true) {
+				if(thread1 && thread2 && thread3 && thread4 && thread5) {
+					System.out.println("End of File");
+					byte[] sendData = new String("-EOF-").getBytes();
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+					serverSocket.send(sendPacket);
+					break;
+				}
+			}					
+		//}	    
+	}
+
+	public static int byteArrayToInt(byte[] b) 
+	{
+	    int value = 0;
+	    for (int i = 0; i < 4; i++) {
+	        int shift = (4 - 1 - i) * 8;
+	        value += (b[i] & 0x000000FF) << shift;
+	    }
+	    return value;
+	}
+
+	public static byte[] intToByteArray(int a)
+	{
+	    byte[] ret = new byte[4];
+	    ret[3] = (byte) (a & 0xFF);   
+	    ret[2] = (byte) ((a >> 8) & 0xFF);   
+	    ret[1] = (byte) ((a >> 16) & 0xFF);   
+	    ret[0] = (byte) ((a >> 24) & 0xFF);
+	    return ret;
+	}
+	
+	public byte[] concatByteArray(byte[] a, byte[] b) throws IOException {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+		outputStream.write( a );
+		outputStream.write( b );
+
+		return outputStream.toByteArray( );
 	}
 }
